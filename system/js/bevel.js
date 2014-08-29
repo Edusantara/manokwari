a61602cc01418fef52e15e0a100cc46153e20835
mijortsa <astrojim.blankon@gmail.com>
2014-08-29 21:33:04 +0700

    html panel sosmed

---
+++ system/js/bevel.js
@@ -0,0 +1,60 @@
+function RunPustekkom() {
+	Utils.run_command("exo-open --launch WebBrowser %u http://setjen.kemdikbud.go.id/pustekkom");
+}
+function RunSuaraEdukasi() {
+	Utils.run_command("exo-open --launch WebBrowser %u http://suaraedukasi.kemdikbud.go.id");
+}
+function RunFacebook() {
+	Utils.run_command("exo-open --launch WebBrowser %u https://facebook.com/pustekkom");
+}
+function RunTwitter() {
+	Utils.run_command("exo-open --launch WebBrowser %u https://twitter.com/pustekkom");
+}
+
+// gnome control center
+
+function RunWallpaper() {
+	Utils.run_command("gnome-control-center background");
+}
+function RunAccount() {
+	Utils.run_command("gnome-control-center user-accounts");
+}
+function RunSound() {
+	Utils.run_command("gnome-control-center sound");
+}
+function RunInfo() {
+	Utils.run_command("gnome-control-center info");
+}
+function RunBluetooth() {
+	Utils.run_command("gnome-control-center bluetooth");
+}
+function RunRegional() {
+	Utils.run_command("gnome-control-center region");
+}
+function RunKeyboard() {
+	Utils.run_command("gnome-control-center keyboard");
+}
+function RunPower() {
+	Utils.run_command("gnome-control-center power");
+}
+function RunDate() {
+	Utils.run_command("gnome-control-center datetime");
+}
+function RunDisplay() {
+	Utils.run_command("gnome-control-center display");
+}
+function RunMouse() {
+	Utils.run_command("gnome-control-center mouse");
+}
+function RunNetwork() {
+	Utils.run_command("gnome-control-center network");
+}
+function RunOnline() {
+	Utils.run_command("gnome-control-center online-accounts");
+}
+function RunPrinter() {
+	Utils.run_command("gnome-control-center printers");
+}
+function RunShare() {
+	Utils.run_command("gnome-control-center sharing");
+}
